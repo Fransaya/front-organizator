83cfcd33eb545de3576921147b3060e67eac4d3c
Francisco Sayago <fransayago04@gmail.com>
2024-02-18 10:47:35 -0300

    register finalizado

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,6 +7,12 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { CommonModule, NgIf } from '@angular/common';
 
+//? SERVICIO DE AUTHENTICACION
+import { AuthService } from '../../Core/Services/auth/auth.service';
+
+import Swal from 'sweetalert2';
+
+//? INTERFACE DE RESPUESTA
 @Component({
   selector: 'app-register',
   standalone:true,
@@ -16,7 +22,7 @@ import { CommonModule, NgIf } from '@angular/common';
 })
 export class RegisterComponent {
 
-  constructor(private route: Router, ){}
+  constructor(private route: Router,private authServices: AuthService ){}
 
   registerForm=new FormGroup({
     usu_nombre: new FormControl('',Validators.required),
@@ -24,12 +30,51 @@ export class RegisterComponent {
     usu_contraseña:new FormControl('',Validators.required)
   });
 
-  public RegisterUser():void{
-    console.log("valores del formulario", this.registerForm.value)
-    
+  public RegisterUser():any{
+    const usuario= this.registerForm.value.usu_nombre!;
+    const correo= this.registerForm.value.usu_email!;
+    const contraseña=this.registerForm.value.usu_contraseña!;
+    if(this.valiidationDates(usuario, correo, contraseña)){
+      try{
+        this.authServices.register(usuario, correo, contraseña).subscribe({
+          next:(res:any)=>{
+            if(res.succes){
+              localStorage.setItem('token',res.data.token);
+              localStorage.setItem('session',res.data.userId);
+              this.route.navigate(['dashboard']);
+            }else{
+              Swal.fire("Error","Succedio un error al registrarse", "error");
+            }
+          },
+          error:(err)=>{
+            console.error("Error al registrar usuario",err);
+            throw err;
+          }
+        })
+      }catch(err){
+        console.error("Error interno del servidor");
+      }
+    }
     //* reseteo del formulario
     // this.registerForm.reset();
   };
+
+  public valiidationDates(usuario:string, email:string, contraseña:string):any{
+    if(usuario!=="" && usuario){
+      const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if(email!=="" && email && regexEmail.test(email)){
+        if(contraseña!=="" && contraseña){
+          return true;
+        }else{
+          return "La contraseña es invalida"
+        }
+      }else{
+        return "El correo electronico es invalido"
+      }
+    }else{
+      return "Debe ingresar un nombre de usuario"
+    }
+  }
 
   public irAInicioSesion(){
     this.route.navigate(['login']);
