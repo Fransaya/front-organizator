3ad44f59a1323dd2ea8e099d01d1114c8a62eff3
Francisco Sayago <fransayago04@gmail.com>
2024-02-26 16:03:21 -0300

    cambios y notas terminadas

--- src/app/notas/modal-modificar-nota/modificar-nota-modal/modificar-nota-modal.component.ts
+++ src/app/notas/modal-modificar-nota/modificar-nota-modal/modificar-nota-modal.component.ts
@@ -3,54 +3,58 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NotasService } from '../../../../Core/Services/notas/notas.service';
 import Swal from 'sweetalert2';
+import { MessageService } from 'primeng/api';
 
 
 @Component({
   selector: 'app-modificar-nota-modal',
   templateUrl: './modificar-nota-modal.component.html',
-  styleUrl: './modificar-nota-modal.component.css'
+  styleUrl: './modificar-nota-modal.component.css',
+  providers:[MessageService]
 })
 export class ModificarNotaModalComponent implements OnInit {
-
+  //* FORMULARIO PARA MODIFICAR NOTAS
   public form: FormGroup;
+  //* VARIABLE PARA ALMACENAR NOTAS
   id_nota:number=0;
 
-  constructor( public dialogRef: MatDialogRef<ModificarNotaModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data:any,
-    private fb:FormBuilder, private notasService: NotasService){
+  constructor( public dialogRef: MatDialogRef<ModificarNotaModalComponent>, @Inject(MAT_DIALOG_DATA) public data:any,private fb:FormBuilder, 
+  private notasService: NotasService,private messageService: MessageService){
       this.form=this.fb.group({
         titulo:['', Validators.required],
         contenido:['', Validators.required],
       })
     }
-
+    //? ---------------------------------------------------- FUNCIONALIDADES ----------------------------------------------------
   ngOnInit(): void {
     this.form.patchValue(this.data);
     //? guardo el id de la nota para realizar la modificacion
     this.id_nota=this.data.id;
   };
-
+  //* CIERRO MODAL
   onNoClick(): void {
     this.dialogRef.close();
   }
-
+  //* CERRAR MODAL AL CANCELAR (BTN)
+  public cancelarNota(){
+    this.dialogRef.close();
+  }
+  //! ---------------------------------------------------- PATCH NOTA ----------------------------------------------------
   public modificarNota(){
-    console.log("valores del form", this.form.value, "id de la nota", this.id_nota);
     try{
       const titulo= this.form.value.titulo;
       const contenido= this.form.value.contenido;
       if(titulo && contenido){
         this.notasService.modificarNota(this.id_nota, titulo,contenido).subscribe({
           next:(response)=>{
-            console.log("respuesta de nota modificada", response);
             if(response){
+              this.messageService.add({ severity: 'success', summary: 'Modificada', detail: 'Nota modificada.' });
               this.dialogRef.close();
-              Swal.fire("Nota Modificada", "","success");
-              
             }
           },  
           error:(err)=>{
-            console.error("Erro del servidor", err)
+            console.error("Erro del servidor", err);
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al modificar.' });
           }
         })
       }
@@ -58,10 +62,4 @@ export class ModificarNotaModalComponent implements OnInit {
       console.error("Error al modificar la nota", err);
     }
   }
-
-  public cancelarNota(){
-    this.dialogRef.close();
-  }
-  
-  
 }
--- src/app/notas/notas.component.ts
+++ src/app/notas/notas.component.ts
@@ -2,97 +2,93 @@ import { Component, OnInit,inject, TemplateRef } from '@angular/core';
 import { FormBuilder,  } from '@angular/forms';
 import {  FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
-import Swal from 'sweetalert2';
 import moment from 'moment';
 import { MatDialog } from '@angular/material/dialog';
-
+import { MessageService } from 'primeng/api';
 
 
 //? SERVICIO DE NOTAS
 import { NotasService} from '../../Core/Services/notas/notas.service';
+//? MODELO DE NOTAS
 import { Notas } from '../../Core/models/notas.model';
+//? MODAL DE MODIFICAR NOTAS
 import { ModificarNotaModalComponent } from './modal-modificar-nota/modificar-nota-modal/modificar-nota-modal.component';
-import { DialogRef } from '@angular/cdk/dialog';
 
 
 @Component({
   selector: 'app-notas',
   templateUrl: './notas.component.html',
   styleUrl: './notas.component.css',
-  
+  providers:[MessageService]
   
 })
 export class NotasComponent implements OnInit {
-
+  //* MODELO DE NOTAS
   notas: Notas[] | any=[];
-
-
-
+  //* FORM PARA MODIFICAR NOTA
   public notaForm: FormGroup = this.formBuilder.group({});
 
-  constructor(private formBuilder: FormBuilder, private notasService: NotasService, public dialog:MatDialog){ }
+  constructor(private formBuilder: FormBuilder, private notasService: NotasService, public dialog:MatDialog,private messageService: MessageService){ }
 
   ngOnInit(): void {
     this.initFomr();
     this.getNotas();
   }
-
+  //? --------------------------------------------------- FUNCIONALIDADES ---------------------------------------------------
+  //* INICIALIZACION DE FORMULARIO
   public initFomr():void{
     this.notaForm=this.formBuilder.group({
       titulo:['',[Validators.required]],
       contenido:['',[Validators.required]]
     })
-  }
-
-  public getNotas(){
-    try{
-      this.notasService.getNotas().subscribe({
-        next:(response:any)=>{
-          console.log("notas traidas", response)
-          this.notas= response.data;
-        },
-        error:(err)=>{
-          console.error("Error al traer las notas ", err)
-        }
-      })
-    }catch(err){
-      console.error("Error al traer las notas", err)
-    }
-  }
-
-  //* funcion para formatear fechas:
+  };
+  //* FORMATEO DE FECHA (DD-MM-YYYY)
   formateDate(fechaString: string): string {
     const fechaFormateada = moment(fechaString).format('DD-MM-YYYY');
     return fechaFormateada;
   }
-  
-  //? funcion para crear nota
+  //! --------------------------------------------------- METODOS DE NOTAS ---------------------------------------------------
+  //? --------------------------------------------------- GET NOTAS ---------------------------------------------------
+  public getNotas(){
+    try{
+      this.notasService.getNotas().subscribe({
+        next:(response:any)=>{
+          this.notas= response.data;
+        },
+        error:(err)=>{
+          console.error("Error al traer las notas ", err);
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error obtener notas.' });
+        }
+      })
+    }catch(err){
+      console.error("Error interno del servidor", err);
+    }
+  }
+  //? --------------------------------------------------- POST NOTA ---------------------------------------------------
   public crearNota():void{
       try{
-        console.log("valor de form", this.notaForm.value)
         let titulo: string= this.notaForm.value.titulo;
         let descripcion: string =this.notaForm.value.contenido;
-
         if(titulo!="" && descripcion!==""){
           this.notasService.crearNota( titulo, descripcion).subscribe({
-            next:(response)=>{
-              console.log("respuesta a crear notas", response);
+            next:()=>{
               this.notaForm.reset();
-              Swal.fire("Nota","Creada Correctamente","success");
               this.getNotas();
+              this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Nota creada.' });
             },
             error:(err)=>{
-              console.log("respuesta de error", err)
+              console.error("respuesta de error", err);
+              this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al crear nota.' });
             }
           })
         }
       }catch(err){
-        console.error("Error interno del servidor",err)
+        console.error("Error interno del servidor",err);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error del servidor.' });
       }
   };
-
+  //? --------------------------------------------------- PATCH NOTAS (MODAL)---------------------------------------------------
   public modificarNota(id:number, titulo:string,contenido:string){
-    console.log("datos", id, titulo, contenido)
     try{
       if( titulo && contenido){
         //? guardo los datos dentro de un objeto para pasar al modal
@@ -107,36 +103,35 @@ export class NotasComponent implements OnInit {
           });
           //? cuando se cierra el modal se actualizar las notas disponibles
           dialog.afterClosed().subscribe(result=>{
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Nota modificada.' });
             this.getNotas();
           })
         
       }else{
-        console.log("id no disponible");
-        Swal.fire("Error", "No se pudo modificar la nota", "error");
+        this.messageService.add({ severity: 'warn', summary: 'Alerta', detail: 'Complete los campo.' });
       }
     }catch(err){
       console.error("No se pudo modificar la nota", err)
     }
   };
-
+  //? --------------------------------------------------- DELETE NOTA ---------------------------------------------------
   public eliminarNota(id:number){
     try{
       this.notasService.eliminarNota(id).subscribe({
         next:(response)=>{
-          console.log("respuesta al eliminar nota", response)
           if(response){
-            Swal.fire("Nota Eliminada", "","success");
             this.getNotas();
+            this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Nota eliminada.' });
           }
         }, 
         error:(err)=>{
           console.error("Error interno del servidor", err)
-          Swal.fire("Error", "No se pudo eliminar la nota", "error");
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error el eliminar.' });
         }
       })
     }catch(err){
-      console.log("Error al modificar nota", err)
-    }
-  }
-
+      console.error("Error interno del servidor", err);
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error del servidor.' });
+    };
+  };
 }
